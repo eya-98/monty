9eea1b6d50c9915b493b67f610209f197b371134
eya-98 <eya.nani@sesame.com.tn>
2020-12-22 15:53:59 +0000

    verify if n is digit

---
+++ isdigit.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * _isdigit - main function
+ *
+ *@c: variable input
+ *
+ *Return: depends on the outcome of c
+ *
+ */
+int _isdigit(char *c)
+{
+for (i = 0; i < strlen(c); i++)
+{
+if (c[0] = "-")
+i++;
+if (c[i] >= '0' && c[i] <= '9')
+{
+return (1);
+}
+else
+return (0);
+}
+}
