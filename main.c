4ddea1d2496f60a0c68e49fc3ff650e1f3ca6578
eya-98 <eya.nani@sesame.com.tn>
2020-12-22 15:37:17 +0000

    main function

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+stack_t *head, g_head;
+if (argc != 2)
+{
+printf("USAGE: monty file\n");
+exit(EXIT_FAILURE)
+}
+*head = NULL
+g_head = &head: 
+r = _read(argv[1], &head);
+exit(0)  
+}
